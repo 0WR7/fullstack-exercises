fd44c4bf903b415346dacc445fb0646e4a7168fe
owr7 <92971363+0WR7@users.noreply.github.com>
2026-04-09 14:59:45 +0300

    Ver1

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+const App = () => {
+    //state
+    const [];
+
+    return (
+        <div>
+            <p>find countries</p>
+            <input />
+        </div>
+    );
+};
+
+export default App;
---
+++ part2/countries/src/components/Search.jsx
@@ -0,0 +1,5 @@
+const Search = ({ handler }) => {
+    <div>
+        <input onChange={handler} />
+    </div>;
+};
