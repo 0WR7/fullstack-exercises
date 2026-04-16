a1387836ac6064dbeb870600944515b12ead82d5
owr <92971363+0WR7@users.noreply.github.com>
2026-04-16 11:52:19 +0300

    helper functions // tests

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -1,5 +1,33 @@
-const dummy = (blogs) => {
-    return 1;
-}
+const totalLikes = (blogs) => {
+    return blogs.reduce((acc, curr) => acc + curr.likes, 0);
+};
 
-module.exports = {dummy}
+const favoriteBlog = (blogs) => {
+    const favorite = blogs.length
+        ? blogs.reduce((most, blog) => (blog.likes > most.likes ? blog : most))
+        : null;
+    return favorite;
+};
+
+const mostBlogs = (blogs) => {
+    const counts = _.countBy(blogs, "author");
+    const [author, count] = _.maxBy(
+        Object.entries(counts),
+        ([author, count]) => count,
+    );
+
+    return { author, blogs: count };
+};
+
+const mostLikes = (blogs) => {
+    const authors = _.groupBy(blogs, "author");
+
+    const totals = _.map(authors, (personalBlogs, author) => ({
+        author,
+        likes: _.sumBy(personalBlogs, "likes"),
+    }));
+
+    return _.maxBy(totals, "likes");
+};
+
+module.exports = { totalLikes, favoriteBlog, mostBlogs, mostLikes };
